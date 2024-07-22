16fd70443089c7dbebf3de9eff17b08329db2b74
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-22 13:35:47 +0700

    Initial Commit

---
+++ CoreDataDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemoApp
+//
+//  Created by Egor Ukolov on 22.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
