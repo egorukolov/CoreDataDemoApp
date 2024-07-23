0edb8a5f5776164427fc142aab3a9c15dc057880
Egor Ukolov <ukolovegor@yandex.ru>
2024-07-23 12:31:18 +0700

    Подготовка приложения: интерфейс двух экранов без storyboard

--- CoreDataDemoApp/TaskListViewController.swift
+++ CoreDataDemoApp/TaskListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TaskListViewController.swift
 //  CoreDataDemoApp
 //
 //  Created by Egor Ukolov on 22.07.2024.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TaskListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
